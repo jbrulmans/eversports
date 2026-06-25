4979d4824df1222de494c4d4d4d0b34755a2b5cf
Jens Brulmans <brulmansjens@gmail.com>
2026-06-25 21:05:53 +0200

    feat: simplify errors -> handled in zod validation now

--- src/modern/errors/membership.error.ts
+++ src/modern/errors/membership.error.ts
@@ -1,46 +1,9 @@
 import { BaseError } from './base.error';
 
-export class MissingMandatoryFieldsError extends BaseError {
+export class ValidationError extends BaseError {
   readonly status = 400;
-  readonly code = 'missingMandatoryFields';
-}
-
-export class NegativeRecurringPriceError extends BaseError {
-  readonly status = 400;
-  readonly code = 'negativeRecurringPrice';
-}
-
-export class CashPriceExceedsLimitError extends BaseError {
-  readonly status = 400;
-  readonly code = 'cashPriceAbove100';
-}
-
-export class BillingPeriodsExceedMonthlyLimitError extends BaseError {
-  readonly status = 400;
-  readonly code = 'billingPeriodsMoreThan12Months';
-}
-
-export class BillingPeriodsBelowMonthlyMinimumError extends BaseError {
-  readonly status = 400;
-  readonly code = 'billingPeriodsLessThan6Months';
-}
-
-export class BillingPeriodsExceedYearlyLimitError extends BaseError {
-  readonly status = 400;
-  readonly code = 'billingPeriodsMoreThan10Years';
-}
-
-export class BillingPeriodsBelowYearlyMinimumError extends BaseError {
-  readonly status = 400;
-  readonly code = 'billingPeriodsLessThan3Years';
-}
-
-export class InvalidBillingIntervalError extends BaseError {
-  readonly status = 400;
-  readonly code = 'invalidBillingPeriods';
-}
-
-export class InvalidBillingPeriodsError extends BaseError {
-  readonly status = 400;
-  readonly code = 'invalidBillingPeriods';
+  constructor(readonly code: string) {
+    super();
+    this.name = 'ValidationError';
+  }
 }
