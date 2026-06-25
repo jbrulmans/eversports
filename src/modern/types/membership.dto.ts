b4cc6380f32f4e6c7d22d3a31b5cf0edddf02611
Jens Brulmans <brulmansjens@gmail.com>
2026-06-25 13:03:23 +0200

    fix: also make payment method required in DTO
    The README shows that payment method is a string, no | null or ?
    indicator that it should be nullable. Even tho the mock data has
    null fields for it, weird. Just following the contract for now

--- src/modern/types/membership.dto.ts
+++ src/modern/types/membership.dto.ts
@@ -12,7 +12,7 @@ export interface CreateMembershipRequestBody {
 export interface ValidatedMembershipInput {
   name: string;
   recurringPrice: number;
-  paymentMethod: PaymentMethod | null;
+  paymentMethod: PaymentMethod;
   billingInterval: BillingInterval;
   billingPeriods: number;
   validFrom: Date;
